c048c7f3d3240c7db7a8b1eebf95e7dafa5f9ea1
Ositadinma Divine <ositadinmadivine61@gmail.com>
2025-10-11 23:02:44 +0100

    Update auth.js
    Added the API keys

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 // Add your Firebase configuration here
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAQHhtyEAduCAcg6RO2K9J0pegSHs0ldtU",
+  authDomain: "ful-class-skeduling-system.firebaseapp.com",
+  projectId: "ful-class-skeduling-system",
+  storageBucket: "ful-class-skeduling-system.firebasestorage.app",
+  messagingSenderId: "421759719229",
+  appId: "1:421759719229:web:c1c94c43c8e2bab75a1754"
 };
 
 // Initialize Firebase
